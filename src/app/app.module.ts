9436787185422f9cfbe270b6701f2aa8ffa789dd
Alvaro <alvarovalladia@gmail.com>
2023-12-07 18:20:26 +0100

    list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { ButtonModule } from 'primeng/button';
 import { SpeedDialModule } from 'primeng/speeddial';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './views/main/main.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent],
+  declarations: [AppComponent, MainComponent, ListComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -24,6 +26,7 @@ import { MainComponent } from './views/main/main.component';
     ReactiveFormsModule,
     ButtonModule,
     SpeedDialModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
